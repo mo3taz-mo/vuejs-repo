9b94f92fe86dbf8380adbaf237e07be1833496e9
rfathy <ran.fathy@gmail.com>
2019-11-05 11:02:44 +0200

    Styles General

--- src/components/Layout/AppFooter.vue
+++ src/components/Layout/AppFooter.vue
@@ -25,9 +25,10 @@
 </script>
 
 <style lang="scss" scoped>
-    /*#app-footer{
+    #app-footer{
         position: fixed;
         bottom: 0;
         width: 82%;
-    }*/
+        z-index: 2;
+    }
 </style>
