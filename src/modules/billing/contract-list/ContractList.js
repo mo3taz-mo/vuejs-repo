89ed4d52636f10420d2aed241367f280f2d49893
rfathy <ran.fathy@gmail.com>
2019-11-05 17:13:53 +0200

    redraw grid with localstorage data

--- src/modules/billing/contract-list/ContractList.js
+++ src/modules/billing/contract-list/ContractList.js
@@ -1,45 +1,52 @@
 import { EventBus } from '@/services/event-bus.js';
 export default {
     name: "contract-list",
+    temppp: [],
     data() {
-        return { 
+        return {
             formData: [{
                 Code: '45',
                 EnglishName: 'english name',
-                ArabicName:"اسم عربى",
-                Description:'Bla bla bla bla',
+                ArabicName: "اسم عربى",
+                Description: 'Bla bla bla bla',
                 ActivationStatus: false
             },
             {
                 Code: '44',
                 EnglishName: 'english name',
-                ArabicName:"اسم عربى",
-                Description:'Bla bla bla bla',
+                ArabicName: "اسم عربى",
+                Description: 'Bla bla bla bla',
                 ActivationStatus: false
-            }], 
+            }],
         }
     },
     created() {
-        
+
     },
     methods: {
-        deleteHandler: function(e) {
+        deleteHandler: function (e) {
             e.preventDefault();
             $(e.currentTarget).closest("tr").remove();
-
-        },
-        bindData: function() {
-            EventBus.$on('update-data', (receivedData) => {
-                this.formData.push(receivedData);
-                console.log(this.formData);
-                var dataSource = new kendo.data.DataSource({ data: this.formData });
-                dataSource.read();
-                // var gantt = this.$refs.gantt.kendoWidget();            
-                // gantt.formData.add(receivedData)
-            });
-
-            // Initial read of the items in to the data source
-            
         }
+    },
+    mounted: function () {
+        if ($("#grid") && localStorage.tempData) {
+            var allData = {
+                EnglishName: JSON.parse(localStorage.tempData).EnglishName, 
+                ArabicName: JSON.parse(localStorage.tempData).ArabicName,
+                Code: JSON.parse(localStorage.tempData).Code,
+                Description: JSON.parse(localStorage.tempData).Description,
+                ActivationStatus: false
+            };
+            var drawData = this.formData.concat(allData);
+            //debugger
+            $("#grid").data("kendoGrid").dataSource.data(drawData);
+        }
+        EventBus.$on('update-data', (receivedData) => {
+            localStorage.tempData = JSON.stringify(receivedData);
+        });
+    },
+    computed: {
+
     }
 }
